1db5bef091dfe1b501fdf31b0064add5d360ee15
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-11 20:19:24 -0500

    Add actions/strong params to create new User and Cloud resources

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -3,4 +3,19 @@ class CloudsController < ApplicationController
     @clouds = Cloud.all
     render json: @clouds
   end
+
+  def create
+    @cloud = Cloud.new(cloud_params)
+    if @cloud.save
+      render json: @cloud.to_json
+    else 
+      render json: @cloud.errors
+    end
+  end
+
+  private
+
+  def cloud_params
+    params.require(:cloud).permit(:id, :user_id, :label, :words)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users.to_json(only: [:id, :name, :created_at, :updated_at])
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password)
+  end
 end
