f4b1ceda638c5f6d0138c8c61c3c590eada96954
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-12 12:38:30 -0500

    Add React Router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 import Search from './components/Search';
+
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
       <div className="App">
         <Navbar />
         <h1>Hello!</h1>
         <Search />
       </div>
-    );
-  }
+  );
 }
+
+const Nav = () => {
+  return (
+    <Navbar>
+      <h1>hey</h1>
+    </Navbar>
+  )
+}
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Route path="/" render={Nav} />
+      <Route exact path="/app" render={App}/>
+    </React.Fragment>
+  </Router>),
+  document.getElementById('root')
+)
 
 export default App;
