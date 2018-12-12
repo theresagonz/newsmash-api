1dac5b746f411c2f6cff12844ee019fa3c3e4ffc
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-11 23:46:29 -0500

    Move html file to own folder, remove references to turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#index'
+  root to: 'app#index'
   scope '/api' do
     resources :users
     resources :clouds
