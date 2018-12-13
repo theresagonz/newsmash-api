baffb2067ba8beb66b2233bed7f81ab526d7fbe7
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-13 13:09:45 -0500

    Add info rendered by Story component

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Story = props => {
   return (
-    <div>
-      <h4>{props.story.title}</h4>
+    <div >
+      <a href={props.story.url}><img src={props.story.urlToImage} alt="news photo" /></a>
+      <a href={props.story.url}><h5>{props.story.title}</h5></a>
+      <p>{props.story.source.name}</p>
+      <p>{props.story.content} </p>
     </div>
   )
 }
