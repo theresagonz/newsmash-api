c81733a1fc228d2597b56058140ea948aa392718
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-12 20:41:48 -0500

    Move Navbar to its own component

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import Search from './components/Search';
+import Navbar from './components/Navbar';
 
-import { Navbar, NavItem, NavDropdown, MenuItem, Jumbotron, Button } from 'react-bootstrap';
+import { MenuItem, Jumbotron, Button } from 'react-bootstrap';
 
 const App = () => {
   return (
@@ -17,37 +18,10 @@ const App = () => {
   );
 }
 
-const Nav = () => {
-  return (
-    <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#home">React-Bootstrap</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
-        </NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.4}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-    </Navbar>
-  )
-}
-
 ReactDOM.render((
   <Router>
     <React.Fragment>
-      {/* <Route path="/" render={Nav} /> */}
+      <Route path="/" render={Navbar} />
       <Route exact path="/app" render={App}/>
     </React.Fragment>
   </Router>),
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <div className="navbar-text">
+          <a href="/">Home</a>
+          <a href="/app/">About</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
