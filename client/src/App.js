d6ab33313974db37e90dbea9eb9132a908b64b2f
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-17 01:32:47 -0500

    Move router from index.js to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import StoriesContainer from './containers/StoriesContainer';
 import WelcomeContainer from './containers/WelcomeContainer';
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <WelcomeContainer />
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={WelcomeContainer} />
+          <Route path="/headlines" component={StoriesContainer} />
+        </Switch>
+      </React.Fragment>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,27 +3,21 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import thunk from 'redux-thunk';
 
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/index';
 import './index.css';
 import App from './App';
-import StoriesContainer from './containers/StoriesContainer';
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/" render={App} />
-        <Route path="/headlines" render={StoriesContainer} />
-      </Switch>
-    </Router>
+    <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
