0a1a0c780c10767c13f8760d61bdcdedf6c7dd00
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-10 18:18:19 -0500

    Fix buggy conditional, reformat for readability

--- app/models/mash.rb
+++ app/models/mash.rb
@@ -5,16 +5,27 @@ class Mash < ApplicationRecord
 
   def self.filter_text(text)
     # Filter out news source names and other extra text
-    text.gsub(/\WAP\W/, '').gsub(/[A-Z]{2,}[,]\s[a-zA-Z]{1,}./, '').gsub(/WASHINGTON/, '').gsub('Read Full Article at RT.com', '').gsub(/\WReuters\W/, '').gsub('-', ' ').gsub('\'s', '')
+    text
+      .gsub(/\WAP\W/, '')
+      .gsub(/[A-Z]{2,}[,]\s[a-zA-Z]{1,}./, '')
+      .gsub(/WASHINGTON/, '')
+      .gsub('Read Full Article at RT.com', '')
+      .gsub(/\WReuters\W/, '')
+      .gsub('-', ' ')
+      .gsub('\'s', '')
   end
 
   def self.getMashString(data)
     text = data.map do |story|
-      desc = story.description
+      description = story.description
       # Add punctuation for better analytics
-      desc + '.' if desc[-1,1] != '.' || desc [-1,1] != '?'
-    end.join('')
-
+        if description[-1,1] != '.' && description[-1,1] != '?' && description[-1,1] != '…'
+          description + '.'
+        else
+          description
+        end
+    end.join(' ')
+    
     self.filter_text(text)
   end
 end
