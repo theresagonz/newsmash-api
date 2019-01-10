a1c36135dc9994ce59fb4cbef3a251c35559fd8b
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-09 20:29:03 -0500

    Add check for empty words array

--- client/src/components/Mash.js
+++ client/src/components/Mash.js
@@ -11,56 +11,58 @@ const PROPTYPES = {
   ),
 };
 
+const mashConfig = words => {
+  return {
+    type: 'wordcloud',
+    options: {
+      words: words,
+      stepRadius: 55,
+      stepAngle: 10,
+      // dataurl: 'resources/cachedchart.json',
+      cache: {
+        data: true,
+      },
+      style: {
+        fontFamily: 'Zilla Slab',
+        hoverState: {
+          backgroundColor: 'white',
+          borderColor: 0,
+          fontColor: 'black',
+          textAlpha: 1,
+        },
+      },
+      colorType: 'palette',
+      palette: [
+        '#2196F3',
+        '#3F51B5',
+        '#42A5F5',
+        '#5C6BC0',
+        '#64B5F6',
+        '#7986CB',
+        '#90CAF9',
+        '#9FA8DA',
+        '#BBDEFB',
+        '#C5CAE9',
+      ],
+    },
+  };
+};
+
 export default class Mash extends Component {
   render() {
-    const mashConfig = {
-      type: 'wordcloud',
-      options: {
-        words: this.props.words,
-        stepRadius: 55,
-        stepAngle: 10,
-        width: '100%',
-        height: '100%',
-        // dataurl: 'resources/cachedchart.json',
-        cache: {
-          data: true,
-        },
-        style: {
-          fontFamily: 'Zilla Slab',
-          hoverState: {
-            backgroundColor: 'white',
-            borderColor: 0,
-            fontColor: 'black',
-            textAlpha: 1,
-          },
-        },
-        colorType: 'palette',
-        palette: [
-          '#2196F3',
-          '#3F51B5',
-          '#42A5F5',
-          '#5C6BC0',
-          '#64B5F6',
-          '#7986CB',
-          '#90CAF9',
-          '#9FA8DA',
-          '#BBDEFB',
-          '#C5CAE9',
-        ],
-      },
-    };
-
-
-    return (
-      <div>
-        <WordMash
-          id="mash-canvas"
-          height="400"
-          width="100%"
-          data={mashConfig}
-        />
-      </div>
-    );
+    if (this.props.words.length) {
+      return (
+        <div>
+          <WordMash
+            id="mash-canvas"
+            height="400"
+            width="100%"
+            data={mashConfig(this.props.words)}
+          />
+        </div>
+      );
+    }
+    return <div />;
   }
 }
 
