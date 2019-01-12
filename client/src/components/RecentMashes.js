b655886d8ac0e54e6e12756ae36aa0db389c00ba
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-12 11:02:58 -0500

    Refactor with variables for readability

--- client/src/components/RecentMashes.js
+++ client/src/components/RecentMashes.js
@@ -5,21 +5,26 @@ import _ from 'lodash';
 class RecentMashes extends React.Component {
   state = {};
 
-  handleClick = (id) =>  {
+  handleClick = id => {
     this.setState({
-      [id]: (this.state[id] || 0) + 1
+      [id]: (this.state[id] || 0) + 1,
     });
-  }
+  };
 
   render() {
     const savedMashLinks = this.props.recentMashes
       ? this.props.recentMashes.map(mash => {
+          const path = `/mashes/saved/${mash.id}`;
+          const topicTitleCase = _.startCase(_.replace(mash.topic, /-/g, ' '));
           return (
             <div key={mash.id} className="sidebar-link">
-              <Link to={`/mashes/saved/${mash.id}`}>
-                {`${_.startCase(_.replace(mash.topic, /-/g, ' '))}`}
-              </Link>
-              <span className="like-button" onClick={() => this.handleClick(mash.id)}>&hearts;</span>
+              <Link to={path}>{topicTitleCase}</Link>
+              <span
+                className="like-button"
+                onClick={() => this.handleClick(mash.id)}
+              >
+                &hearts;
+              </span>
               <span>{this.state[mash.id] || 0}</span>
             </div>
           );
