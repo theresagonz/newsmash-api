ca40d8bb761b7eabb8bf87f9ae2e5492435cb2c8
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-12 10:43:38 -0500

    Add mash like button no persistence

--- client/src/components/RecentMashes.js
+++ client/src/components/RecentMashes.js
@@ -2,25 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
-const RecentMashes = props => {
-  const savedMashLinks = props.recentMashes
-    ? props.recentMashes.map(mash => {
-        return (
-          <div key={mash.id} className="sidebar-link">
-            <Link to={`/mashes/saved/${mash.id}`}>
-              {`${_.startCase(_.replace(mash.topic, /-/g, ' '))}`}
-            </Link>
-          </div>
-        );
-      })
-    : null;
+class RecentMashes extends React.Component {
+  state = {};
 
-  return (
-    <div className="sidebar-container">
-      <div className="sidebar-heading">Saved Mashes</div>
-      {savedMashLinks}
-    </div>
-  );
-};
+  handleClick = (id) =>  {
+    this.setState({
+      [id]: (this.state[id] || 0) + 1
+    });
+  }
+
+  render() {
+    const savedMashLinks = this.props.recentMashes
+      ? this.props.recentMashes.map(mash => {
+          return (
+            <div key={mash.id} className="sidebar-link">
+              <Link to={`/mashes/saved/${mash.id}`}>
+                {`${_.startCase(_.replace(mash.topic, /-/g, ' '))}`}
+              </Link>
+              <span className="like-button" onClick={() => this.handleClick(mash.id)}>&hearts;</span>
+              <span>{this.state[mash.id] || 0}</span>
+            </div>
+          );
+        })
+      : null;
+
+    return (
+      <div className="sidebar-container">
+        <div className="sidebar-heading">Saved Mashes</div>
+        {savedMashLinks}
+      </div>
+    );
+  }
+}
 
 export default RecentMashes;
