d96ca4ba4458cee4f0b871d8527aad69aecc20e7
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-21 18:07:02 -0500

    Remove likes feature

--- client/src/components/MashLink.js
+++ client/src/components/MashLink.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
-export default function MashLink({mash}) {
+export default function MashLink({ mash }) {
   const path = `/mashes/saved/${mash.id}`;
   const topicTitleCase = _.startCase(_.replace(mash.topic, /-/g, ' '));
-
   return (
     <div key={mash.id} className="sidebar-link">
       <Link to={path}>{topicTitleCase}</Link>
-      <span className="like-button" onClick={() => this.handleClick(mash.id)}>
-        &hearts;
-      </span>
       <span>{mash.likes}</span>
     </div>
   );
--- client/src/components/RecentMashes.js
+++ client/src/components/RecentMashes.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import MashLink from './MashLink';
 
-class RecentMashes extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      recentMashes: [],
-    };
-  }
-
-  handleClick = id => {
-    this.setState({
-      [id]: (this.state[id] || 0) + 1,
-    });
-  };
-
-  render() {
-    const mashes =
-      this.state.recentMashes.length === 0
-        ? this.props.recentMashes
-        : this.state.recentMashes;
-    const savedMashLinks = mashes.map(mash => (
-      <MashLink mash={mash} />
-    ));
-
-    return (
-      <div className="sidebar-container">
-        <div className="sidebar-heading">Saved Mashes</div>
-        {savedMashLinks}
-      </div>
-    );
-  }
-}
+const RecentMashes = props => { 
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-heading">Saved Mashes</div>
+      {props.recentMashes.map(mash => (
+        <MashLink mash={mash} />
+      ))}
+    </div>
+  );
+};
 
 export default RecentMashes;
