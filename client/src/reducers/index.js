1abcec14ee32fc1ba8745b93e33f8e320abe314a
theresamorelli <theresa.l.morelli@gmail.com>
2019-01-31 17:26:39 -0500

    Move front end to separate repo

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import mixReducer from './mixReducer';
-import mashReducer from './mashReducer';
-
-const rootReducer = combineReducers({
-  mix: mixReducer,
-  mash: mashReducer
-});
-export default rootReducer;
