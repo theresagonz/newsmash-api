23d6997819603c76e21f2937e09d773ab35590da
theresamorelli <theresa.l.morelli@gmail.com>
2019-02-02 18:09:56 -0500

    Change puma default port

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module NewsMash
     # the framework and any gems in your application.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3001', '127.0.0.1:3001',
+        origins 'localhost:3000', '127.0.0.1:3000',
         /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
      
         resource '*',
