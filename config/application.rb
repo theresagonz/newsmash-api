2a98aa096f2cd91911127a929cc7df95da56f6a5
theresamorelli <theresa.l.morelli@gmail.com>
2019-02-03 15:15:40 -0500

    Change local server port to 5000 for express

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module NewsMash
     # the framework and any gems in your application.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', '127.0.0.1:3000', 'http://newsmashed.herokuapp.com'
+        origins 'localhost:5000', '127.0.0.1:5000', 'http://newsmashed.herokuapp.com'
         /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
      
         resource '*',
