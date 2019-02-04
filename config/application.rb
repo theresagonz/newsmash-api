71420bf1817a28859711a85f40cfbe7d153093df
theresamorelli <theresa.l.morelli@gmail.com>
2019-02-03 20:08:11 -0500

    Add 3000 port as allowed CORS origin

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,13 @@ module NewsMash
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Port 3000 for local webpack server
+    # Port 5000 for local express server
+    # http for production
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:5000', '127.0.0.1:5000', 'http://newsmashed.herokuapp.com'
+        origins ['localhost:3000', '127.0.0.1:3000', 'localhost:5000', '127.0.0.1:5000', 'http://newsmashed.herokuapp.com']
         /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
      
         resource '*',
